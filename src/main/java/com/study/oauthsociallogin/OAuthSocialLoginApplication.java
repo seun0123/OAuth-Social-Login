545d110f444522db56858475e1b17999f576a720
Park, seun <seunpark04@gmail.com>
2024-12-18 22:50:43 +0900

    Initial setting

---
+++ src/main/java/com/study/oauthsociallogin/OAuthSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.study.oauthsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthSocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthSocialLoginApplication.class, args);
+    }
+
+}
