a5188983d9d7cbb447d588991a09166b855ace10
Park, seun <seunpark04@gmail.com>
2024-12-22 18:43:07 +0900

    feat: NaverTokenResponse에 사용자 정보 요청 메서드 추가

--- src/main/java/com/study/oauthsociallogin/naver/dto/response/NaverTokenResponse.java
+++ src/main/java/com/study/oauthsociallogin/naver/dto/response/NaverTokenResponse.java
@@ -1,4 +1,4 @@
-package com.study.oauthsociallogin.naver.dto;
+package com.study.oauthsociallogin.naver.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
